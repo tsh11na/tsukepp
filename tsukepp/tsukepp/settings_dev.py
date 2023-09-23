c4a4516beee311099c278ed5ed6f5b763d664e5f
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:27 +0000

    Introduce debug toolbar

--- tsukepp/tsukepp/settings_dev.py
+++ tsukepp/tsukepp/settings_dev.py
@@ -2,13 +2,25 @@
 Django development settings for tsukepp project.
 """
 
+import socket  # For Django Debug Toolbar
 from .settings_common import *
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+INSTALLED_APPS += [
+    "debug_toolbar"
+]
+
+MIDDLEWARE += [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
 
 # Logging
 # https://docs.djangoproject.com/en/4.2/topics/logging/
--- tsukepp/tsukepp/urls.py
+++ tsukepp/tsukepp/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tsuke.urls')),
-    path('accounts/', include('allauth.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('__debug__', include('debug_toolbar.urls'))  # DEBUG
 ]
