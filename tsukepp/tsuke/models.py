259ec8c4a04aff58aad620a1b6e4539a332ede06
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:26 +0000

    Add Meta to model

--- tsukepp/tsuke/models.py
+++ tsukepp/tsuke/models.py
@@ -5,10 +5,14 @@ from django.db import models
 class Tsuke(models.Model):
     """1回のツケ"""
     user = models.ForeignKey(CustomUser, verbose_name="ユーザ", on_delete=models.PROTECT)
-    purchase_date = models.DateTimeField(verbose_name="購入日時", auto_now_add=True)
+    purchase_datetime = models.DateTimeField(verbose_name="購入日時", auto_now_add=True)
     amount = models.PositiveSmallIntegerField(verbose_name="金額")
     is_paid = models.BooleanField(verbose_name="支払", default=False)
     note = models.CharField(verbose_name="メモ", max_length=50, blank=True)
 
+    class Meta:
+        verbose_name = "ツケ"
+        verbose_name_plural = "ツケ"
+
     def __str__(self):
-        return f"{self.purchase_date} ({self.amount})"
+        return f"{self.purchase_datetime} ({self.amount})"
