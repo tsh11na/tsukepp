5f564c6ee1454a88bf8dc4ce034c54f55ea2f290
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:27 +0000

    Add heroku settings

--- tsukepp/tsukepp/settings.py
+++ tsukepp/tsukepp/settings.py
@@ -1,4 +1,26 @@
-from .settings_common import *
+"""Heroku用環境設定"""
 
-# SECURITY WARNING: don't run with debug turned on in production!
+from .settings_common import *
+import dj_database_url
+import django_heroku
+
 DEBUG = False
+
+
+ALLOWED_HOSTS = ['.herokuapp.com']
+
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
+django_heroku.settings(locals())
