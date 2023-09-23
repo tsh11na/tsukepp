41ab8584116bf4612190f6b62ffd0039fd6d6bc9
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:27 +0000

    Update admin

--- tsukepp/tsuke/admin.py
+++ tsukepp/tsuke/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import Tsuke
 
+class TsukeAdmin(admin.ModelAdmin):
+    list_display = ["purchase_datetime", "amount", "user", "note"]
 
-admin.site.register(Tsuke)
+admin.site.register(Tsuke, TsukeAdmin)
