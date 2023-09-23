4b4a3e790f31e756456fdd0adf7c42335db64d22
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:26 +0000

    Update url rooting

--- tsukepp/tsuke/urls.py
+++ tsukepp/tsuke/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = "tsuke"
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index")
+    path("", views.IndexView.as_view(), name="index"),
+    path("create/", views.TsukeCreateView.as_view(), name="create"), # ツケる
+    path("history/", views.TsukeHistoryView.as_view(), name="history"),  # ツケ履歴
 ]
