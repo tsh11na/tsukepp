2a15616459ea622baf2273095288bbbfac9eba5c
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:27 +0000

    Add payment page

--- tsukepp/tsuke/urls.py
+++ tsukepp/tsuke/urls.py
@@ -7,5 +7,6 @@ app_name = "tsuke"
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("create/", views.TsukeCreateView.as_view(), name="create"), # ツケる
+    path("pay/", views.tsuke_pay, name="pay"),  # 支払う
     path("history/", views.TsukeHistoryView.as_view(), name="history"),  # ツケ履歴
 ]
