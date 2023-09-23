401e887fda2de4e368e347850b1cfba93424acb2
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:26 +0000

    Add logging settings

--- tsukepp/tsukepp/settings.py
+++ tsukepp/tsukepp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +126,42 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging
+# https://docs.djangoproject.com/en/4.2/topics/logging/
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "debug",
+            "class": "logging.StreamHandler",
+            "formatter": "dev"
+        }
+    },
+    "formatters": {
+        "dev": {
+            "format": "\t".join([
+                "%(asctime)s",
+                "[%(levelname)s]",
+                "%(pathname)s(Line:%(lineno)d",
+                "%(message)s"
+            ])
+        }
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            # "propagate": True,
+        },
+        "tsuke": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+        "accounts": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        }
+    },
+}
