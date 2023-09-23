6f16e8faf40fb5f16fe6d06001d1fbc1accbecc3
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:26 +0000

    Update views and forms

---
+++ tsukepp/tsuke/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from .models import Tsuke
+
+class TsukeCreateForm(forms.ModelForm):
+    class Meta:
+        model = Tsuke
+        fields=("amount", "note",)
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
--- tsukepp/tsuke/views.py
+++ tsukepp/tsuke/views.py
@@ -1,5 +1,36 @@
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
 
+from .models import Tsuke
+from .forms import TsukeCreateForm
 
 class IndexView(generic.TemplateView):
+    """トップページ"""
     template_name = "tsuke/index.html"
+
+
+class TsukeHistoryView(LoginRequiredMixin, generic.ListView):  # TODO: LoginRequiredMixin
+    """ツケ履歴"""
+    model = Tsuke
+    template_name = 'tsuke/history.html'
+
+
+class TsukeCreateView(LoginRequiredMixin, generic.CreateView):
+    """ツケの登録"""
+    model = Tsuke
+    template_name = "tsuke/create.html"
+    form_class = TsukeCreateForm
+    success_url = reverse_lazy("tsuke:index")
+
+    def form_valid(self, form):
+        tsuke = form.save(commit=False)
+        tsuke.user = self.request.user
+        tsuke.save()
+        messages.success(self.request, "ツケを登録しました。")
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, "ツケの登録に失敗しました。")
+        return super().form_invalid(form)
