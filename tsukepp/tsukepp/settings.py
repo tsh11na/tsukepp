9f6a71054272f6cf47c490e7345cb48f7314be39
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:48:09 +0000

    Fix middleware/database settings

--- tsukepp/tsukepp/settings.py
+++ tsukepp/tsukepp/settings.py
@@ -10,7 +10,29 @@ django_heroku.settings(locals())
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
+]
+
+DATABASES = {
+# https://code.djangoproject.com/ticket/33685
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'name',
+        'HOST': 'host',
+        'USER': 'user',
+        'PORT': '',
+        'PASSWORD': '',
+    }
+}
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
