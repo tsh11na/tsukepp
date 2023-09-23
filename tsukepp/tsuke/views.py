2dff9d7eee6cf25ff400f7e7863d8a39c200155a
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:27 +0000

    Add user info to navbar

--- tsukepp/tsuke/views.py
+++ tsukepp/tsuke/views.py
@@ -1,7 +1,11 @@
+from typing import Any
+from django.db import models
+from django.db.models.query import QuerySet
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.urls import reverse_lazy
+from django.shortcuts import render
 
 from .models import Tsuke
 from .forms import TsukeCreateForm
@@ -10,11 +14,23 @@ class IndexView(generic.TemplateView):
     """トップページ"""
     template_name = "tsuke/index.html"
 
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+    
+        if self.request.user.is_authenticated:
+            context["user"] = self.request.user
+        else:
+            context["user"] = None
+        return context
+
 
 class TsukeHistoryView(LoginRequiredMixin, generic.ListView):  # TODO: LoginRequiredMixin
     """ツケ履歴"""
     model = Tsuke
     template_name = 'tsuke/history.html'
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Tsuke.objects.filter(user=self.request.user).order_by("-purchase_datetime")
 
 
 class TsukeCreateView(LoginRequiredMixin, generic.CreateView):
@@ -34,3 +50,7 @@ class TsukeCreateView(LoginRequiredMixin, generic.CreateView):
     def form_invalid(self, form):
         messages.error(self.request, "ツケの登録に失敗しました。")
         return super().form_invalid(form)
+
+
+def tsuke_pay(request):
+    return render(request, "tsuke/pay.html")
