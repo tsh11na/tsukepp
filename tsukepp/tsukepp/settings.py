ec0f138e337bb50788a87fb60559cd39c8f28b21
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-09-23 10:22:26 +0000

    Add applications

--- tsukepp/tsukepp/settings.py
+++ tsukepp/tsukepp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tsuke.apps.TsukeConfig',
+    'accounts.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
