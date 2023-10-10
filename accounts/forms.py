e125c11e8e98718fe78ebadfc0d5040ef297cf25
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2023-10-10 14:24:36 +0000

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,33 +1,34 @@
 from allauth.account.forms import LoginForm, SignupForm
 from django import forms
 
-from tsukepp.settings_common import TSUKEPP_ACTIVATION_KEY
-
 # ここで書いたフォームは settings.ACCOUNT_FORMS で有効化
 
 
 class CustomSignupForm(SignupForm):
 
     def __init__(self, *args, **kwargs):
-        self.correct_activation_key = kwargs.pop('activation_key', TSUKEPP_ACTIVATION_KEY)
+        try:
+            from tsukepp.settings_common import TSUKEPP_ACTIVATION_KEY
+            self.correct_activation_key = kwargs.pop('activation_key', TSUKEPP_ACTIVATION_KEY)
+        except ImportError:
+            self.correct_activation_key = ''
         super().__init__(*args, **kwargs)
 
     activation_key = forms.CharField(
         max_length=100,
         required=False,
-        label="アクティベーションキー",
+        label="アクティベーションキー（オプション）",
         widget=forms.PasswordInput(attrs={'placeholder': "アクティベーションキー"}),
-        help_text="管理者から提供されたキーがあれば入力してください。"
+        help_text="アカウント作成に必要なキーです。管理者から提供されている場合は入力してください。"
     )
 
     def clean_activation_key(self):
-        activation_key = self.cleaned_data.get('activation_key')
-        correct_activation_key = self.correct_activation_key  # 正しいアクティベーションキーを設定
+        activation_key = self.cleaned_data['activation_key']
 
-        if activation_key and activation_key != correct_activation_key:
-            raise forms.ValidationError('アクティベーションキーが正しくありません。')
+        if activation_key == self.correct_activation_key:
+            return activation_key
 
-        return activation_key
+        raise forms.ValidationError('アクティベーションキーが正しくありません。')
 
 
 class CustomLoginForm(LoginForm):
