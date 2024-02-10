a972018bac7b837a56e1da3985c7f3814843fbe3
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2024-02-10 01:51:28 +0000

    Change admin site header and title

--- tsuke/admin.py
+++ tsuke/admin.py
@@ -11,5 +11,8 @@ class TsukeAdmin(admin.ModelAdmin):
     list_display = ["purchase_date", "amount", "user", "note", "payment_date"]
 
 
+admin.site.site_header = "Tsukepp 管理画面"
+admin.site.index_title = "メニュー"
+
 admin.site.register(ItemCategory, ItemCategoryAdmin)
 admin.site.register(Tsuke, TsukeAdmin)
