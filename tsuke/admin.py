aec2addc550bed65adddf6a1ccc6a6cf926ef03d
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2024-02-10 06:59:14 +0000

    Customize headers in admin pages

--- tsuke/admin.py
+++ tsuke/admin.py
@@ -1,19 +1,49 @@
 from django.contrib import admin
 from django.db.models import Sum
+from django.http.request import HttpRequest
+from django.template.response import TemplateResponse
 
 from .models import ItemCategory, Tsuke, TsukeTotal
 
 
 class ItemCategoryAdmin(admin.ModelAdmin):
     list_display = ["category"]
+    change_list_template = "admin/itemcategory_change_list.html"
 
 
 class TsukeAdmin(admin.ModelAdmin):
-    list_display = ["purchase_date", "amount", "user", "note", "payment_date"]
+    list_display = ["purchase_date", "amount", "user", "note", "is_paid", "payment_date"]
+    readonly_fields = [field.name for field in Tsuke._meta.fields]
+    list_filter = ["user", "category", "is_paid"]
+    change_list_template = "admin/tsuke_change_list.html"  # 一覧画面
+    change_form_template = "admin/tsuke_change_form.html"  # 詳細画面
+
+    def has_add_permission(self, *args, **kwargs):
+        return False
+    
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(request, extra_context)
+        try:
+            qs = response.context_data["cl"].get_queryset(request)
+        except (AttributeError, KeyError):
+            return response
+        
+        # フィルタ条件を取得
+        filters = {
+            param: request.GET.getlist(param) for param in request.GET
+            if not param.startswith("_")
+        }
+
+        response.context_data['filters'] = filters
+        return response
+
 
 class TsukeTotalAdmin(admin.ModelAdmin):
     list_display = ["user"]
     change_list_template = "admin/tsuketotal_change_list.html"
+
+    def has_add_permission(self, *args, **kwargs):
+        return False
 
     def changelist_view(self, request, extra_context=None):
         response = super().changelist_view(request, extra_context)
