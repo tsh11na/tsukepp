ebaab6261533bd01cc36d0b81c94e12f0df0eaa2
Takeru SHIINA <shiina.takeru.w4@s.mail.nagoya-u.ac.jp>
2024-02-10 09:07:23 +0000

    Update admin pages

--- tsuke/admin.py
+++ tsuke/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from django.db.models import Sum
-from django.http.request import HttpRequest
-from django.template.response import TemplateResponse
 
 from .models import ItemCategory, Tsuke, TsukeTotal
+
+
+class PaidFilter(admin.SimpleListFilter):
+    """清算済みフィルタ"""
+    title = "清算"
+    parameter_name = "is_paid"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("paid", "済"),
+            ("unpaid", "未"),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "paid":
+            return queryset.filter(is_paid=True)
+        if self.value() == "unpaid":
+            return queryset.filter(is_paid=False)
 
 
 class ItemCategoryAdmin(admin.ModelAdmin):
@@ -14,20 +30,31 @@ class ItemCategoryAdmin(admin.ModelAdmin):
 class TsukeAdmin(admin.ModelAdmin):
     list_display = ["purchase_date", "amount", "user", "note", "is_paid", "payment_date"]
     readonly_fields = [field.name for field in Tsuke._meta.fields]
-    list_filter = ["user", "category", "is_paid"]
+    list_filter = ["user", "category", PaidFilter]
     change_list_template = "admin/tsuke_change_list.html"  # 一覧画面
     change_form_template = "admin/tsuke_change_form.html"  # 詳細画面
 
-    def has_add_permission(self, *args, **kwargs):
+    def has_add_permission(self, request, obj=None) -> bool:
         """ツケは登録ページからしか追加できない"""
+        return False
+    
+    def has_view_permission(self, request, obj=None) -> bool:
+        if request.user.is_staff:
+            return True  # スタッフは閲覧可能
+        return False
+    
+    def has_delete_permission(self, request, obj=None) -> bool:
+        if request.user.is_superuser:
+            return True  # スーパーユーザのみ削除可能
         return False
     
     def changelist_view(self, request, extra_context=None):
         response = super().changelist_view(request, extra_context)
-        try:
-            qs = response.context_data["cl"].get_queryset(request)
-        except (AttributeError, KeyError):
-            return response
+
+        # try:
+        #     qs = response.context_data["cl"].get_queryset(request)
+        # except (AttributeError, KeyError):
+        #     return response
         
         # フィルタ条件を取得
         filters = {
